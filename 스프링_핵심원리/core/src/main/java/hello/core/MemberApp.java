cd431388fd296d83cbc3358d687797c2fd09e747
jinseop <yeomjinseop@gmail.com>
2022-08-11 16:05:18 +0900

    Java: Member Domain + Order Domain

---
+++ 스프링_핵심원리/core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args){
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP); // 1뒤에 L은 Long 타입이기 때문에 붙여줘야 한다. 안붙이면 컴파일 에러난다.
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("findMember = " + findMember.getName());
+        System.out.println("member = " + member.getName());
+        System.out.println(findMember.equals(member));
+    }
+}

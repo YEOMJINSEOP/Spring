e187a49013db9b91127b6bbf77998410c40364cd
jinseop <yeomjinseop@gmail.com>
2022-08-12 15:55:05 +0900

    Spring: AppConfig

--- 스프링_핵심원리/core/src/main/java/hello/core/MemberApp.java
+++ 스프링_핵심원리/core/src/main/java/hello/core/MemberApp.java
@@ -7,7 +7,9 @@ import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args){
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+//      MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP); // 1뒤에 L은 Long 타입이기 때문에 붙여줘야 한다. 안붙이면 컴파일 에러난다.
         memberService.join(member);
 

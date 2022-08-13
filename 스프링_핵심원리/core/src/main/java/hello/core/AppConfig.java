d93d1a519f442c6884b5fb17fd5c4ba9a8850b70
jinseop <yeomjinseop@gmail.com>
2022-08-14 00:08:15 +0900

    Spring: Find Bean Test

--- 스프링_핵심원리/core/src/main/java/hello/core/AppConfig.java
+++ 스프링_핵심원리/core/src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-
-
+@Configuration
+//@Configuration 설정정보
 public class AppConfig {
 
+    @Bean
+    //@Bean을 각 메서드에 붙이면 Spring Container에 등록이 된다.
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
--- 스프링_핵심원리/core/src/main/java/hello/core/MemberApp.java
+++ 스프링_핵심원리/core/src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args){
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-//      MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // AppConfig에 있는 환경 설정 정보를 가지고 Spring이 Spring Container에 넣어 관리해준다.
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP); // 1뒤에 L은 Long 타입이기 때문에 붙여줘야 한다. 안붙이면 컴파일 에러난다.
         memberService.join(member);
 
